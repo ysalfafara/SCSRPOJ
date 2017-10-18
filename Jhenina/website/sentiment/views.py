9dafa3e880013b0eec13ad038200c0fc1d66d287
jtstacruz <jheninasta.cruz@gmail.com>
2017-10-18 22:33:28 +0400

    Creating another view for wordcloud

--- Jhenina/website/sentiment/views.py
+++ Jhenina/website/sentiment/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1> Taal Vista Sentiment Reviews</h1>")
+
+def detail(request, review_id):
+    return HttpResponse("<h2>Wordcloud for Reviews: " + str(review_id) + "</h2>")
