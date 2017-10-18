714ae8af662a22dbbe11ea6e8430a9bf216662af
jtstacruz <jheninasta.cruz@gmail.com>
2017-10-18 22:05:36 +0400

    Database test: Filtering data

--- Jhenina/website/sentiment/models.py
+++ Jhenina/website/sentiment/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 
 class ReviewSite(models.Model):
-    reviews = models.CharField(max_length=2000)
+    reviews = models.CharField(max_length=5000)
     rating_date = models.CharField(max_length=250)
     rating = models.CharField(max_length=100)
     site = models.CharField(max_length=100)
     sentiment = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.reviews + ' - ' + self.sentiment
+
+
