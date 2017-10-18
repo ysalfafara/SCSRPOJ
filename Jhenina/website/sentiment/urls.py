3df0dfcc5427b6da21e5867829a282de0f0c21e0
jtstacruz <jheninasta.cruz@gmail.com>
2017-10-18 22:38:10 +0400

    Added return key to urls.py

--- Jhenina/website/sentiment/urls.py
+++ Jhenina/website/sentiment/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /sentiment/
     url(r'^$', views.index, name='index'),
+
+    # /sentiment/1/
+    url(r'^(?P<review_id>[0-9]+)$', views.detail, name='detail'),
 ]
