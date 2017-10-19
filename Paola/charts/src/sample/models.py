286a4cbd45a33086b7f0123eb14caf5135173901
PaolaOrtega <pfortega@outlook.com>
2017-10-19 14:24:43 +0800

    Charts.js connected to django (data manually inputted to code)

---
+++ Paola/charts/src/sample/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class ReviewSites(models.Model):
+    site = models.CharField(max_length=15)
+    review = models.TextField(max_length=5000)
+    date = models.DateField()
+    sentiment = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.review
+
