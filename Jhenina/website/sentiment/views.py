7afd27642c9e17fff6da53f140a831f4702cbc4a
jtstacruz <jheninasta.cruz@gmail.com>
2017-10-25 14:44:33 +0400

    Deleted html code in views.py

--- Jhenina/website/sentiment/views.py
+++ Jhenina/website/sentiment/views.py
@@ -3,11 +3,7 @@ from .models import ReviewSite
 
 def index(request):
     all_ReviewSite = ReviewSite.objects.all()
-    html = ''
-    for ReviewSite in all_ReviewSite:
-        url = '/sentiment/' + str(ReviewSite.id) + '/'
-        html += '<a href="' + url + ReviewSite.ReviewSite_reviews + '</a><br>'
-    return HttpResponse(html)
+    return HttpResponse('')
 
 def detail(request, review_id):
     return HttpResponse("<h2>Wordcloud for Reviews: " + str(review_id) + "</h2>")
