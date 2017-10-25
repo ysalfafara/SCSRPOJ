9914914d884be86d3dc32e4933b83dd154484a5b
jtstacruz <jheninasta.cruz@gmail.com>
2017-10-25 14:43:18 +0400

    Testing html code in django

--- Jhenina/website/sentiment/views.py
+++ Jhenina/website/sentiment/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from .models import ReviewSite
 
 def index(request):
-    return HttpResponse("<h1> Taal Vista Sentiment Reviews</h1>")
+    all_ReviewSite = ReviewSite.objects.all()
+    html = ''
+    for ReviewSite in all_ReviewSite:
+        url = '/sentiment/' + str(ReviewSite.id) + '/'
+        html += '<a href="' + url + ReviewSite.ReviewSite_reviews + '</a><br>'
+    return HttpResponse(html)
 
 def detail(request, review_id):
     return HttpResponse("<h2>Wordcloud for Reviews: " + str(review_id) + "</h2>")
+
